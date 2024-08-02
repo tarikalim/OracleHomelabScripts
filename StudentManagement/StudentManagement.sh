06491a8061f9856c8cfd2e4112885b43fae607bc
tarikalim <tarikalim1899@gmail.com>
2024-08-02 19:17:42 +0300

    TC no must be unique

--- StudentManagement/StudentManagement.sh
+++ StudentManagement/StudentManagement.sh
@@ -1,116 +1,114 @@
 #!/bin/bash
 
-# Kullanıcıdan giriş bilgilerini al
-read -p "T.C. Kimlik Numaranızı giriniz: " tc
-read -sp "Şifrenizi giriniz: " password
+read -p "Enter your T.C no: " tc
+read -sp "Enter your password: " password
 echo
 
-# Kullanıcıyı doğrula
 user_info=$(grep "^$tc," users.txt)
 
 if [ -z "$user_info" ]; then
-    echo "Kullanıcı bulunamadı."
+    echo "Nou user."
     exit 1
 fi
 
-# Kullanıcının şifresini doğrula
 stored_password=$(echo "$user_info" | cut -d ',' -f4)
 
 if [ "$stored_password" == "$password" ]; then
-    echo "Giriş başarılı!"
-    # Kullanıcının rolünü al
+    echo "Login success!"
     user_role=$(echo "$user_info" | cut -d ',' -f5)
-    echo "Rolünüz: $user_role"
+    echo "ROLE: $user_role"
 else
-    echo "Hatalı şifre."
+    echo "Invalid password."
     exit 1
 fi
 
-# Ana menüye dönmek için bekleme fonksiyonu
 wait_for_keypress() {
-    read -n 1 -s -r -p "Ana menüye dönmek için herhangi bir tuşa basın..."
+    read -n 1 -s -r -p "Press any key to go main menu"
     echo
 }
 
-# Admin işlemleri
 admin_menu() {
     while true; do
-        echo "1) Öğrenci Ekle"
-        echo "2) Tüm Öğrencileri Görüntüle"
-        echo "3) Öğrenci Bilgilerini Görüntüle"
-        echo "4) Öğrenci Bilgilerini Güncelle"
-        echo "5) Öğrenciyi Sil"
-        echo "6) Çıkış"
-        read -p "Bir seçenek giriniz: " choice
+        echo "1) Add student"
+        echo "2) List students"
+        echo "3) View student info"
+        echo "4) Update student info"
+        echo "5) Delete student"
+        echo "6) Exit"
+        read -p "What do you want to do ?: " choice
 
         case $choice in
             1)
                 read -p "TC: " new_tc
-                read -p "Ad: " new_name
-                read -p "Soyad: " new_surname
-                read -sp "Şifre: " new_password
+                existing_student=$(grep "^$new_tc," users.txt)
+                if [ -n "$existing_student" ]; then
+                  echo "Error: A student with this T.C no already in the system."
+                  wait_for_keypress
+                  else
+                read -p "Name: " new_name
+                read -p "Surname: " new_surname
+                read -sp "Password: " new_password
                 echo
                 echo "$new_tc,$new_name,$new_surname,$new_password,normal" >> users.txt
-                echo "Öğrenci başarıyla eklendi."
+                echo "Student added."
                 wait_for_keypress
+                fi
                 ;;
             2)
-                echo "Tüm öğrenciler:"
+                echo "Student List:"
                 cat users.txt
                 wait_for_keypress
                 ;;
             3)
-                read -p "TC'sini girdiğiniz öğrencinin bilgilerini görüntüleyin: " search_tc
+                read -p "Enter student T.C no: " search_tc
                 student_info=$(grep "^$search_tc," users.txt)
                 if [ -z "$student_info" ]; then
-                    echo "Öğrenci bulunamadı."
+                    echo "No student for given T.C no."
                 else
-                    echo "Öğrenci bilgileri: $student_info"
+                    echo "Student info: $student_info"
                 fi
                 wait_for_keypress
                 ;;
             4)
-                read -p "Güncellemek istediğiniz öğrencinin TC'sini giriniz: " update_tc
+                read -p "Enter student T.C to update info: " update_tc
                 student_info=$(grep "^$update_tc," users.txt)
                 if [ -z "$student_info" ]; then
-                    echo "Öğrenci bulunamadı."
+                    echo "No student."
                 else
-                    read -p "Yeni ad: " new_name
-                    read -p "Yeni soyad: " new_surname
-                    read -sp "Yeni şifre: " new_password
+                    read -p "Name: " new_name
+                    read -p "Surname: " new_surname
+                    read -sp "Password: " new_password
                     echo
                     sed -i "/^$update_tc,/c\\$update_tc,$new_name,$new_surname,$new_password,normal" users.txt
-                    echo "Öğrenci bilgileri güncellendi."
+                    echo "Update success."
                 fi
                 wait_for_keypress
                 ;;
             5)
-                read -p "Silmek istediğiniz öğrencinin TC'sini giriniz: " delete_tc
+                read -p "Enter T.C no to delete student from system: " delete_tc
                 sed -i "/^$delete_tc,/d" users.txt
-                echo "Öğrenci silindi."
+                echo "Student deleted."
                 wait_for_keypress
                 ;;
             6)
-                echo "Çıkış yapılıyor..."
+                echo "Logout..."
                 exit 0
                 ;;
             *)
-                echo "Geçersiz seçenek."
+                echo "Invalid option."
                 wait_for_keypress
                 ;;
         esac
     done
 }
 
-# Normal kullanıcı işlemleri
 normal_menu() {
-    echo "Kullanıcı bilgileri:"
+    echo "User information:"
     echo "$user_info"
-    echo "Çıkış yapılıyor..."
+    echo "Logout..."
     exit 0
 }
 
-# Rolüne göre menüyü göster
 if [ "$user_role" == "admin" ]; then
     admin_menu
 else
